61cd6c85566a78e01d783a588f303e208e847003
MarioMatheus <mariomatheu@gmail.com>
2019-10-02 16:29:35 -0300

    Remove main.spec file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@
 import { Add } from './parsers/expression'
 import { logAst } from './utils/logger'
 
-// const ast = Add.parse('-1 / -(4 + 2)')
-const ast = Add.parse('2-5')
+const ast = Add.parse('-1 / -(4 + 2)')
+// const ast = Add.parse('2-5')
 logAst(ast, true)
