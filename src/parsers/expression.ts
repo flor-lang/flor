da2780fb5bc457377e61b42c45c8c364a4480f4d
MarioMatheus <mariomatheu@gmail.com>
2019-10-03 19:13:23 -0300

    Added production comments to expressions, previous commit - bug fixed: left recursion fixed at expressions productions

--- src/parsers/expression.ts
+++ src/parsers/expression.ts
@@ -12,6 +12,10 @@ export type AddParser = P.Parser<P.Node<'add', {}>>
 export type RelParser = P.Parser<P.Node<'rel', {}>>
 export type EqualityParser = P.Parser<P.Node<'equality', {}>>
 
+/**
+ * Parse Integers Numbers and Expressions between parenthesis
+ * factor -> (add) | number
+*/
 export const Factor: FactorParser = P
   .alt(
     P.seqObj(
@@ -25,6 +29,10 @@ export const Factor: FactorParser = P
   .node('factor')
 
 const UnaryLine = AddOperator
+/**
+ * Parse Unary Numbers with + or - operators
+ * unary -> +unary | -unary | factor
+*/
 export const Unary: UnaryParser = P
   .seqObj(
     P.alt(UnaryLine, P.optWhitespace).named('unaryline'),
@@ -41,6 +49,10 @@ const TermLine: ObjectParser = P
     ),
     P.optWhitespace
   )
+/**
+ * Parse terms with factor operators(* / %)
+ * term -> term * unary | term / unary | term % unary | unary
+*/
 export const Term: TermParser = P
   .seqObj(
     Unary.named('unary'),
@@ -58,6 +70,10 @@ const AddLine: ObjectParser = P
     ),
     P.optWhitespace
   )
+/**
+ * Parse terms with add operators(+ -)
+ * add -> add + term | add - term | term
+*/
 export const Add: AddParser = P
   .seqObj(
     Term.named('term'),
@@ -72,6 +88,10 @@ const Inequality: ObjectParser = P
     RelOperator.named('operator'), P.optWhitespace,
     Add.named('rhs')
   )
+/**
+ * Parse inequalities relations (> < >= <=) between expressions
+ * rel -> rel < add | rel > add | rel >= add | rel <= add | add
+*/
 export const Rel: RelParser = P
   .alt(
     Inequality,
@@ -88,6 +108,10 @@ export const EqualityLine: ObjectParser = P
     ),
     P.optWhitespace
   )
+/**
+ * Parse equalities relations (== !=) between expressions
+ * equality -> equality == rel | equality != rel | rel
+*/
 export const Equality: EqualityParser = P
   .seqObj(
     Rel.named('rel'),
