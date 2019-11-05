52449ba3f42b9230addcdbd25bab11b01ec41d71
Rafael Coelho <rafaellcoellho@gmail.com>
2019-11-04 21:44:15 -0300

    some progress

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,14 +1,53 @@
 #!/usr/bin/env node
 import * as Yargs from 'yargs'
+import * as fs from 'fs'
+import * as glob from 'glob'
+
+import { Assignment } from '../parsers/assignment'
+import { logAst } from '../utils/logger'
+import { Result, Node } from 'parsimmon'
 
 Yargs
   .scriptName('tinp')
-  .usage('Usage: $0 [options] [.tinp file]')
-  .example('$0 hello.tinp', '')
-  .version('v')
-  .alias('v', 'version')
-  .help('h')
-  .alias('h', 'help')
-  .epilog('Created by Mario Matheus and Rafael Coelho.')
+  .usage('Exemplo de uso: $0 [opção] [arquivo .tinp]')
+  .example('$0 oi_mundo.tinp', '')
+  .version(false)
+  .help('m')
+  .alias('m', 'manual')
+  .epilog('Autores: Mario Matheus e Rafael Coelho.')
+  .locale('pt_BR')
 
-const path = Yargs.argv._[0]
+Yargs
+  .options({
+    saida: {
+      default: 'js',
+      describe: 'saida do compilador',
+      choices: ['js', 'ast']
+    }
+  })
+
+const files = Yargs.argv._[0] ? [Yargs.argv._[0]] : glob.sync('**/*.tinp')
+const filesContent = files
+  .map((path): string => {
+    try {
+      const content = fs.readFileSync(path, 'utf-8')
+      return String(content)
+    } catch (e) {
+      console.log(`Não foi possível ler arquivo: ${path}`)
+      process.exit(1)
+    }
+  })
+
+if (Yargs.argv.saida === 'js') {
+  console.log('Ainda não implementado :(')
+} else if (Yargs.argv.saida === 'tab-sim') {
+  console.log('Ainda não implementado')
+} else if (Yargs.argv.saida === 'ast') {
+  const asts = filesContent.map((fc): Result<Node<'assignment', {}>> => {
+    return Assignment.parse(fc)
+  })
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  asts.forEach(ast => {
+    logAst(ast, true)
+  })
+}
