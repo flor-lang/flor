2bb51c341bb240247216da4cc164233aeb070e91
rafaellcoellho <rafaellcoellho@gmail.com>
2019-12-12 15:59:13 -0300

    test single and multi line comment

---
+++ tests/comments.spec.ts
@@ -0,0 +1,18 @@
+import comments from '../src/utils/comments'
+
+test('remove single line comment', (): void => {
+  expect(
+    comments.remove('a = 1// teste')
+  ).toBe('a = 1')
+})
+
+test('remove multi line comment', (): void => {
+  // Multi line comment in one line
+  expect(
+    comments.remove('a = 1/* teste */')
+  ).toBe('a = 1')
+  // Multi line comment in more than one line
+  expect(
+    comments.remove('a = 1\n/*\nteste\n*/')
+  ).toBe('a = 1\n')
+})
