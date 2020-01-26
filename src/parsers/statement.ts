75eb06f54572faed67c6e4fb4cdd35874a0610c7
MarioMatheus <mariomatheu@gmail.com>
2020-01-26 02:16:17 -0300

    Implemented provisional if statement

--- src/parsers/statement.ts
+++ src/parsers/statement.ts
@@ -1,10 +1,25 @@
 import * as P from 'parsimmon'
 import '../utils/parsimmon-extension'
+import { If, Then, End } from './operators'
+import { Expression } from './expression'
+import { Assignment } from './assignment'
 
+export type IfStatementParser = P.Parser<P.Node<'if', {}>>
 export type StatementParser = P.Parser<P.Node<'statement', {}>>
+
+export const IfStatement: IfStatementParser = P
+  .seqObj(
+    If,
+    Expression.named('condition'),
+    Then,
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    P.lazy((): StatementParser => Statement).named('then'), P.optWhitespace,
+    End
+  )
+  .node('if')
 
 export const Statement: StatementParser = P
   .alt(
-
+    Assignment
   )
   .node('statement')
--- tests/statements.spec.ts
+++ tests/statements.spec.ts
@@ -1,10 +1,35 @@
 import { canParse, cantParse } from './utils'
-import { Statement } from '../src/parsers/statement'
+import { Statement, IfStatement } from '../src/parsers/statement'
+
+test('if statement', (): void => {
+  const canParseIfStatement = canParse(IfStatement)
+  const cantParseIfStatement = cantParse(IfStatement)
+
+  canParseIfStatement([
+    'se peso < 0 entao peso = 0 fim',
+    'se (altura > 1.80 e peso > 80) entao mensagem = "Voce é bem grande" fim',
+    'se ativo entao ativo = falso fim',
+    'se divida_atual - valor_pago > 0 entao mensagem = "Ainda restam contas a pagar" fim ',
+    `
+      se ativo entao
+        ativo = falso
+      fim
+    `
+  ])
+  cantParseIfStatement([
+    'se fim entao se fim',
+    'se peso < 0 entao peso = 0',
+    'se peso < 0 peso = 0 fim',
+    'se ativo entao fim',
+    'se fim entao a = 0 fim',
+    'se a = 0 entao a = 0 fim'
+  ])
+})
 
 test('parse statement', (): void => {
-  const canParseFactor = canParse(Statement)
-  const cantParseFactor = cantParse(Statement)
+  const canParseStatement = canParse(Statement)
+  const cantParseStatement = cantParse(Statement)
 
-  canParseFactor([])
-  cantParseFactor([])
+  canParseStatement([])
+  cantParseStatement([])
 })
