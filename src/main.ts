55fb470a0d10e3bf1217aa838c0e9b3244359894
rafaellcoellho <rafaellcoellho@gmail.com>
2020-02-29 17:14:15 -0300

    playground uses program, a high level parser

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 // Playground code
-import { Expression } from './parsers/expressions'
-import { Assignment } from './parsers/assignment'
+import { Program } from './parsers/program'
 // import { logAst } from './utils/logger'
 import { traverser } from './utils/traverse'
 
-Expression.parse('(-1 / -(4 + 2)) >= 1 + 4 ou 1 + 2')
-
-const ast = Assignment.parse('message = "Hello World!"')
+const ast = Program.parse('message = "Hello World!"')
 // logAst(ast, true)
 const visitor = {
-  assignment: {
+  program: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     enter (node: any, parent: any): void {
       console.log(`opa, entrou gostoso ${node} ${parent}`)
