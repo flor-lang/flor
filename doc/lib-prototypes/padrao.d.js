5e2bc0ed30272c122f5737f490a32d5ab15397da
MarioMatheus <mariomatheu@gmail.com>
2020-03-02 21:30:21 -0300

    added prototype to standard method of string type

---
+++ doc/lib-prototypes/padrao.d.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-expressions */
+/* Basic Types */
+
+// Texto : String
+'string'.charAt(0)
+'string'.charCodeAt(0)
+'string'.concat(['hei', 'listen'])
+'string'.endsWith('sufixo')
+'string'.includes('palavra')
+'string'.indexOf('texto')
+'string'.lastIndexOf('texto')
+'string'.length // array property
+'string'.match(new RegExp('[0-9]*')) // regex lib
+'string'.normalize()
+'string'.replace('valor', 'novo valor')
+'string'.search('valor' || new RegExp('valor'))
+
+'string'.substring(0, 10) || 'string'.slice(0, 10) // array property
+'string'.split('-')
+'string'.startsWith('prefixo')
+'string'.toLowerCase()
+'string'.toUpperCase()
+'string'.trim()
+'string'.trimLeft()
+'string'.trimRight()
