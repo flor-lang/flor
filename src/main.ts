af2d8bb11810dd6dfaa842de8fe5378060e103b6
rafaellcoellho <rafaellcoellho@gmail.com>
2020-03-28 17:05:41 -0300

    update playground

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import { logAst } from './utils/logger'
 import { Program } from './parsers/program'
 
-const ast = Program.parse(`
-  telefones[ 0 ] = ( 1 + 2 )
-`)
+// const ast = Program.tryParse(`i = 0`)
+const ast = Program.parse(`i = 0`)
 
+// console.log(JSON.stringify(ast, null, 4))
 logAst(ast, true)
