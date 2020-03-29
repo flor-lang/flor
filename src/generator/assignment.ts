b5d620e428ee165b7010a4140cc28a75fffd5334
rafaellcoellho <rafaellcoellho@gmail.com>
2020-03-29 01:23:49 -0300

    .

--- src/generator/assignment.ts
+++ src/generator/assignment.ts
@@ -1,20 +1,26 @@
 import { AstNode } from 'utils/traverse'
 
-interface AssignmentNode { lhs: LhsNode; rhs: AstNode }
-interface LhsNode { subscriptable: AstNode; params: [] }
+interface AssignmentNode { lhs: LhsNode; rhs: RhsNode }
+interface LhsNode { subscriptable: { value: AstNode }; params: [] }
+interface RhsNode { value: {} }
 
 export const assignmentCG = (node: AstNode): string => {
   const assignmentNode = (node.value as unknown) as AssignmentNode
 
   const lhs = (node: LhsNode): string => {
     let lhsCode = ''
-    const subscriptable = node.subscriptable as AstNode
-    const subNode = subscriptable.value as AstNode
+    const subscriptable = node.subscriptable
+    const subNode = subscriptable.value
 
     if (subNode.name === 'identifier') {
       lhsCode += subNode.value
     } else if (subNode.name === 'function-call') {
-      // TODO
+      // const identifier = subNode.value.identifier
+      // lhsCode += identifier + '('
+      // const args = subNode.value['labeled-args']
+      // args.forEach(a => {
+      //   lhsCode += a
+      // })
     }
 
     // node.params.forEach(param => {
@@ -24,7 +30,7 @@ export const assignmentCG = (node: AstNode): string => {
     return lhsCode
   }
 
-  const rhs = (node: AstNode): string => {
+  const rhs = (node: RhsNode): string => {
     const expression = node.value as AstNode
     const literal = expression.value as AstNode
     // TO DO
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 // Playground code
 import { logAst } from './utils/logger'
 import { Program } from './parsers/program'
+// import { codeGenerator } from './generator/main'
+// import { AstNode } from './utils/traverse'
 
-// const ast = Program.tryParse(`i = 0`)
-const ast = Program.parse(`i = 0`)
+// const astTeste = Program.parse(`i = 0`)
+const ast = Program.parse(`
+  i = 0
+  j = 2
+  getValues(valor: 0) = 10
+`)
+// console.log(codeGenerator(ast))
 
 // console.log(JSON.stringify(ast, null, 4))
 logAst(ast, true)
