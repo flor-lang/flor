550bcc8846a1c11dbde7e94c820449aeef56fe96
MarioMatheus <mariomatheu@gmail.com>
2020-04-04 08:38:52 -0300

    added block visitor with first table business

---
+++ src/visitor/program.ts
@@ -0,0 +1,25 @@
+import { AstNode } from './traverse'
+import Env from '../enviroment/env'
+import SymbolTable from '../enviroment/symbol-table'
+
+let saved: SymbolTable = null
+
+const block = {
+  enter (node: AstNode, parent: AstNode): void {
+    if (parent.name !== 'program') {
+      saved = Env.get().symbolTable
+      Env.get().symbolTable = new SymbolTable(saved)
+      Env.get().codeOutput += ' {\\n'
+    }
+  },
+  exit (node: AstNode, parent: AstNode): void {
+    if (parent.name !== 'program') {
+      Env.get().symbolTable = saved
+      Env.get().codeOutput += '\\n}'
+    }
+  }
+}
+
+export default {
+  block
+}
--- src/visitor/visitor.ts
+++ src/visitor/visitor.ts
@@ -3,8 +3,10 @@ import { Visitor } from './traverse'
 import assignment from './assignment'
 import literals from './literals'
 import operators from './operators'
+import program from './program'
 
 export const visitor: Visitor = {
+  ...program,
   ...assignment,
   ...literals,
   ...operators
