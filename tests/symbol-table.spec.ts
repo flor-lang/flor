7e15b8cfea4d00a75515a538a1c7520bc8f010dc
MarioMatheus <mariomatheu@gmail.com>
2020-04-04 19:33:26 -0300

    more and more tests

--- tests/symbol-table.spec.ts
+++ tests/symbol-table.spec.ts
@@ -26,3 +26,34 @@ test('test simple redeclaration in program node', () => {
   expect(symbolTable.get('var2')).toMatchObject({ value: 'value2' })
 })
 
+test('test get of symbol in parent scope', () => {
+  const parentSymbolTable = new SymbolTable(null)
+  parentSymbolTable.put('var1', mockedNode('value1'))
+  
+  const symbolTable = new SymbolTable(parentSymbolTable)
+  symbolTable.put('var2', mockedNode('value2'))
+
+  expect(symbolTable.get('var2')).toMatchObject({ value: 'value2' })
+  expect(symbolTable.get('var1')).toMatchObject({ value: 'value1' })
+  expect(parentSymbolTable.get('var2')).toBe(null)
+})
+
+test('test different symbols access to tables with same parent', () => {
+  const parentSymbolTable = new SymbolTable(null)
+  parentSymbolTable.put('var1', mockedNode('value1'))
+  
+  const symbolTable1 = new SymbolTable(parentSymbolTable)
+  symbolTable1.put('var2', mockedNode('value2'))
+
+  const symbolTable2 = new SymbolTable(parentSymbolTable)
+  symbolTable2.put('var3', mockedNode('value3'))
+
+  expect(symbolTable1.get('var1')).toMatchObject({ value: 'value1' })
+  expect(symbolTable2.get('var1')).toMatchObject({ value: 'value1' })
+
+  expect(symbolTable1.get('var2')).toMatchObject({ value: 'value2' })
+  expect(symbolTable2.get('var3')).toMatchObject({ value: 'value3' })
+
+  expect(symbolTable1.get('var3')).toBe(null)
+  expect(symbolTable2.get('var2')).toBe(null)
+})
