42e9be3ca24bb1de962891d47dd2feaf4bab373f
MarioMatheus <mariomatheu@gmail.com>
2020-04-07 20:40:48 -0300

    updated assingment code generator test

--- tests/generators/assignment.spec.ts
+++ tests/generators/assignment.spec.ts
@@ -1,13 +1,23 @@
 import Env from '../../src/enviroment/env'
 import { visitor } from '../../src/backend/visitor'
 import { traverser } from '../../src/backend/traverse'
+import { Assignment } from '../../src/parsers/assignment'
 import { Program } from '../../src/parsers/program'
 import { generatorTester } from '../utils'
 
 test('test assignment', () => {
-  const tryGenerateExpressions = generatorTester(Program, true, 0)
+  const tryGenerateExpressions = generatorTester(Assignment)
 
   tryGenerateExpressions([
-    ['variavel = 1', 'variavel = 1\\n']
+    ['variavel = 1', 'variavel = 1\\n'],
+    ['variavel = verdadeiro', 'variavel = true\\n'],
+  ])
+});
+
+test('test assignment with program', () => {
+  const tryGenerateExpressions = generatorTester(Program)
+
+  tryGenerateExpressions([
+    ['id = 0 variavel = id', 'id = 0\\nvariavel = id\\n']
   ])
 });
