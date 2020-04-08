71a7deb7063024dc316df94c5a8c9f2295a0765a
MarioMatheus <mariomatheu@gmail.com>
2020-04-08 15:15:38 -0300

    refactored locUse analyser

--- src/backend/semantics/definitions.ts
+++ src/backend/semantics/definitions.ts
@@ -1,17 +1,15 @@
 import { AstNode } from '../../backend/traverse'
 import { locSubscriptableIsIdentifier, identifierValueOfLocNode } from '../../utils/aux-functions'
 import Env from '../../enviroment/env'
+import Analyser from './analyser'
 
-export const evaluateLocUse = (node: AstNode): void => {
+const locUse = (node: AstNode): void => {
   if (locSubscriptableIsIdentifier(node)) {
     const identifier = identifierValueOfLocNode(node)
     if (Env.get().symbolTable.get(identifier) === null) {
-      throw new Error(`
-      Módulo de Erro | TODO
-        Váriavel '${identifier}' não foi definida
-        Linha: ${node.start.line}
-        Coluna: ${node.start.column}
-      `)
+      Analyser.throwError(`Váriavel '${identifier}' não foi definida`, node)
     }
   }
 }
+
+export const evaluateLocUse = Analyser.create(locUse)
