93f207a2262d76ea0584b09b8b8b81c42a4d3b0e
MarioMatheus <mariomatheu@gmail.com>
2020-04-24 08:21:32 -0300

    made must be tests

--- tests/backend/semantics.spec.ts
+++ tests/backend/semantics.spec.ts
@@ -1,8 +1,25 @@
 import { semanticTester } from "../utils"
 
 test('test variable definition and your use', () => {
-  const shouldThrows = semanticTester(true, /.*Váriavel '.+' não foi definida/)
-  shouldThrows([
+  const mustBeAllowed = semanticTester(false)
+  const mustThrows = semanticTester(true, /.*Váriavel '.+' não foi definida/)
+
+  mustBeAllowed([
+    `valor = 5
+    cinco = valor`,
+    `valor = 5
+    valor = valor`,
+    `valor = 5
+    se valor > 0 entao
+      valor = 10
+    fim`,
+    'em_linha = (x) := 2*x',
+    `hey_listen = funcao (parametro)
+      retornar "Hey " + parametro + " listen!"
+    fim`
+  ])
+
+  mustThrows([
     'valor = teste', 'valor = valor', 'cinco = cinco', 'in = () := 2*x',
     `dois = 2
     dez = dois + oito`,
@@ -18,8 +35,24 @@ test('test variable definition and your use', () => {
 })
 
 test('test function call use', () => {
-  const shouldThrows = semanticTester(true, /.*Função '.+' não foi definida/)
-  shouldThrows([
+  const mustBeAllowed = semanticTester(false)
+  const mustThrows = semanticTester(true, /.*Função '.+' não foi definida/)
+
+  mustBeAllowed([
+    `ola = () := "Olá Mundo!"
+    ola()
+    `,
+    `hey = (x) := x
+    listen = hey(x: 0)
+    `,
+    `duplicar = (x) := 2*x
+    se 5 < 10 entao
+      duplicar(x: 10)
+    fim
+    `
+  ])
+
+  mustThrows([
     'valor()', 'duplicar(x: 10)', 'treta(x: 5, y: 10)',
     `duplicar = (x) := 2*x
     triplicar(x: 10)`,
