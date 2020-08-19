035092a6e08b3f165da16cbbffb83b042244b410
MarioMatheus <mariomatheu@gmail.com>
2020-08-18 22:23:55 -0300

    created function and added a optional parameter to include identifiers at compile process

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { traverser, AstNode } from './backend/traverse'
 import Env from './enviroment/env'
 import SymbolTable from './enviroment/symbol-table'
 import { FlorCompilationErrorMessage, FlorRuntimeErrorMessage } from './utils/errors'
-import { StandardLib } from './lib/standard.flib'
+import { StandardLib, EmptyAstNode } from './lib/standard.flib'
 import comments from './utils/comments'
 
 export { logAst, logSymbolTable } from './utils/logger'
@@ -19,6 +19,12 @@ export { SymbolTable, AstNode, FlorRuntimeErrorMessage }
  */
 export const parseCode = (code: string): AstNode => Program.tryParse(code)
 
+const loadAbstractLib = (identifiers: string[] = []): void => {
+  identifiers.forEach((identifier: string): void => {
+    Env.get().symbolTable.put(identifier, EmptyAstNode)
+  })
+}
+
 const loadStandardLib = (callbackfn: (identifier: string, node: AstNode) => void): void => {
   for (const key in StandardLib) {
     // eslint-disable-next-line no-prototype-builtins
@@ -28,11 +34,12 @@ const loadStandardLib = (callbackfn: (identifier: string, node: AstNode) => void
   }
 }
 
-const traverseAstFrom = (code: string, toLoadStandardLib = false): void => {
+const traverseAstFrom = (code: string, toLoadStandardLib = false, abstractLib: string[] = []): void => {
   Env.get().clean('prod')
   if (toLoadStandardLib) {
     loadStandardLib((identifier, node): void => Env.get().symbolTable.put(identifier, node))
   }
+  loadAbstractLib(abstractLib)
   const executableCode = comments.remove(code)
   traverser(parseCode(executableCode), visitor)
 }
@@ -43,8 +50,8 @@ const traverseAstFrom = (code: string, toLoadStandardLib = false): void => {
  * @param toLoadStandardLib Indicates whether to include the standard library in analysis process
  * @returns Object SymbolTable
  */
-export const getSymbolTable = (code: string, toLoadStandardLib = false): SymbolTable => {
-  traverseAstFrom(code, toLoadStandardLib)
+export const getSymbolTable = (code: string, toLoadStandardLib = false, abstractLib: string[] = []): SymbolTable => {
+  traverseAstFrom(code, toLoadStandardLib, abstractLib)
   const table = Env.get().symbolTable
   loadStandardLib((identifier): AstNode => table.rm(identifier))
   Env.get().clean('prod')
@@ -57,8 +64,8 @@ export const getSymbolTable = (code: string, toLoadStandardLib = false): SymbolT
  * @param toLoadStandardLib Indicates whether to include the standard library in analysis process
  * @returns JS generated code
  */
-export const compile = (code: string, toLoadStandardLib = false): string => {
-  traverseAstFrom(code, toLoadStandardLib)
+export const compile = (code: string, toLoadStandardLib = false, abstractLib: string[] = []): string => {
+  traverseAstFrom(code, toLoadStandardLib, abstractLib)
   const output = Env.get().getCodeOutputPolyfilled()
   Env.get().clean('prod')
   return output
@@ -71,9 +78,9 @@ export const compile = (code: string, toLoadStandardLib = false): string => {
  * @param toLoadStandardLib Indicates whether to include the standard library in analysis process
  * @returns Object { success: boolean; result: string }
  */
-export const tryCompile = (code: string, toLoadStandardLib = false): { success: boolean; result: string } => {
+export const tryCompile = (code: string, toLoadStandardLib = false, abstractLib: string[] = []): { success: boolean; result: string } => {
   try {
-    const result = compile(code, toLoadStandardLib)
+    const result = compile(code, toLoadStandardLib, abstractLib)
     return { success: true, result }
   } catch (error) {
     const result = FlorCompilationErrorMessage(error)
