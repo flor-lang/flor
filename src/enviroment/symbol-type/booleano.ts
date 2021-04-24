88c8ac8f1499d32715061c9536eea79f1d448806
MarioMatheus <mariomatheu@gmail.com>
2021-04-24 16:58:58 -0300

    remove unusual files

--- src/enviroment/symbol-type/booleano.ts
+++
@@ -1,7 +0,0 @@
-import { SymbolType } from "../symbol-type";
-
-export class BooleanoFlorType extends SymbolType<unknown> {
-  name = 'booleano'
-}
-
-export const BooleanoFlor = new BooleanoFlorType()
--- src/enviroment/symbol-type/dicionario.ts
+++
@@ -1,9 +0,0 @@
-import { QualquerFlorType, SymbolType } from "../symbol-type";
-
-export type DicionarioTypeParameter = { valueType: QualquerFlorType };
-
-export class DicionarioFlorType extends SymbolType<DicionarioTypeParameter> {
-  name = 'dicionario'
-}
-
-export const DicionarioFlor = new DicionarioFlorType()
